5ad9bfe7e37ae63def1676fce9c9a594897c99b0
Alexandre Bléron <alex.bleron@gmail.com>
2018-09-13 00:05:31 +0200

    schedule

--- src/frame/sched.rs
+++ src/frame/sched.rs
@@ -6,6 +6,8 @@
 //! * creates and schedules renderpasses
 //!
 
+use std::collections::HashMap;
+
 use super::*;
 
 use petgraph::algo::toposort;
@@ -14,14 +16,56 @@ impl<'ctx> Frame<'ctx>
 {
     pub fn schedule(&mut self)
     {
-        // issue: this is not an optimal toposort: we want to minimize the lifetime of resources
-        // so that they can be aliased.
-        // Solution: Directed Minimum Linear Arrangement of a Directed Graph
+        // We want a "good" toposort that minimizes the lifetimes of resources
+        // to increase memory aliasing.
+        // This is the "Directed Minimum Linear Arrangement" (Directed minLA) algorithm.
+        // However, finding an optimal solution is NP-hard,
+        // so instead,  do a stable toposort of the graph and hope that
+        // the user did not add tasks in a weird order.
+        // In most cases, the toposort shouldn't change anything.
 
         let sorted = toposort(&self.graph, None).expect("Dependency graph has cycles");
-        // print some info about the graph.
-        info!("Frame info:");
 
+        /*let mut node_x = HashMap::new();
+
+        // init positions
+        for (i,s) in sorted.iter().enumerate() {
+            node_x[s] = i as f32;
+        }
+
+        // damping
+        let K = 0.1;
+
+        // calculate forces
+        let mut forces = vec![0.0;positions.len()];
+        for (i,n) in sorted.iter().enumerate() {
+            let x = i as f32;
+            for nadj in self.graph.neighbors(n) {
+                // calc \delta_x outgoing
+                let delta_x = x - node_x[nadj];
+                forces[i] = - K * delta_x;
+            }
+        }*/
+
+        //
+        //  b -----> a
+        //  1        2
+        //  delta_pos = 2 - 1 = 1
+        //  F = -k \delta_x =
+        //
+
+        // compute forces and update positions
+
+
+        //self.graph.
+
+        info!("Frame info:");
+        info!("Initial:");
+        for n in self.graph.node_indices() {
+            let task = self.graph.node_weight(n).unwrap();
+            info!("  {}(#{})", task.name, n.index());
+        }
+        info!("Sorted:");
         for n in sorted.iter() {
             let task = self.graph.node_weight(*n).unwrap();
             info!("  {}(#{})", task.name, n.index());
